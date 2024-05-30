be2699f680eea74a3dd785ca0d4b3f86d2322b60
MartaGriner <168266736+MartaGriner@users.noreply.github.com>
2024-05-30 17:20:38 +0200

    Delete Ćwiczenia/save_comment.php

--- Ćwiczenia/save_comment.php
+++
@@ -1,8 +0,0 @@
-<?php
-$comment = $_POST['comment'];
-if ($comment) {
-    $comment = htmlspecialchars($comment); // Sanityzacja danych
-    $file = 'comments.txt';
-    file_put_contents($file, $comment . "\n", FILE_APPEND);
-}
-?>
